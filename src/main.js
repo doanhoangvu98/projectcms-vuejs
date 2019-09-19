57cb7fe0b89e0cc24241f3f965dd22cc130fdb1d
doanhoangvu98 <doanhuuanh4@gmail.com>
2019-09-19 16:04:49 +0700

    Create page login admin with vuejs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,57 @@
 import Vue from 'vue'
+import Vuex from 'vuex';
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import LoginPage from "./views/Login.vue"
+import AdminPage from "./views/Admin.vue"
+import Axios from 'axios'
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import store from "./store"
+
+Vue.use(Buefy)
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
+Vue.prototype.$http = Axios;
+const accessToken = localStorage.getItem('accessToken');
+if(accessToken){
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = accessToken;
+}
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/login', component: LoginPage },
+    { 
+      path: '/admin', 
+      component: AdminPage, 
+      name: 'admin',
+      meta: {
+        requiresAuth: true
+      }
+    },
+    { path: '*', redirect: '/' }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
+}) 
+
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2 class="is-title">Admin page</h2>
+    <span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'admin',
+      computed: {
+        isLoggedIn: function(){
+          return this.$store.getters.isLoggedIn
+        }
+      },
+      methods: {
+        logout: function(){
+          this.$store.dispatch('logout')
+          .then(() => {
+            this.$router.push({ path: '/login'});
+          })
+        }
+      }
+  }
+</script>
